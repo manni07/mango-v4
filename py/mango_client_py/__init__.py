65c92dc971a2843018f7149148f09a99330e85ec
manni07 <abend_schnack@yahoo.de>
2024-12-16 16:54:27 +0100

    Update __init__.py
    Implementation of modular structure

--- py/mango_client_py/__init__.py
+++ py/mango_client_py/__init__.py
@@ -1,1 +1,8 @@
-# mango_client_py package
+# mango_client_py/accounts/__init__.py
+
+from .mango_account import MangoAccounts
+from .oracles import Oracles
+from .bank import Bank
+from .group import Group
+
+__all__ = ['MangoAccounts', 'Oracles', 'Bank', 'Group']
