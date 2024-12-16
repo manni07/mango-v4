53a17285728d427d2a3db4df10b5b4f2125ec14f
manni07 <abend_schnack@yahoo.de>
2024-12-16 14:28:24 +0100

    Update bank.py
    Updates for client access

--- py/mango_client_py/accounts/bank.py
+++ py/mango_client_py/accounts/bank.py
@@ -1,1 +1,19 @@
 
+# mango_client_py/accounts/bank.py
+
+from dataclasses import dataclass
+
+@dataclass
+class Bank:
+    # Bank spezifische Felder
+    pass
+
+@dataclass
+class MintInfo:
+    # MintInfo spezifische Felder
+    pass
+
+@dataclass
+class TokenIndex:
+    # TokenIndex spezifische Felder
+    pass
